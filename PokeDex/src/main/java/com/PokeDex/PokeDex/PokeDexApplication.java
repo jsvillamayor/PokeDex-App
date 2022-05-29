f5ab60b6fc798df7acd91693c15768541a1ae1d2
David <levant28269@outlook.com>
2022-05-28 22:07:48 -0400

    this is our project

---
+++ PokeDex/src/main/java/com/PokeDex/PokeDex/PokeDexApplication.java
@@ -0,0 +1,13 @@
+package com.PokeDex.PokeDex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeDexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeDexApplication.class, args);
+	}
+
+}
