b5a028631ab6ffb7f1914819d965ea75e0e5abe3
David <levant28269@outlook.com>
2022-06-01 23:12:22 -0400

    basic file

---
+++ demoPokemon/src/test/java/com/examplePokemon/demoPokemon/DemoPokemonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplePokemon.demoPokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPokemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
