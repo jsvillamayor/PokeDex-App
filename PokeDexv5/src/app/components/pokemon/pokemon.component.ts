3af83138194f70ddd53a50d9ec39771b651ed831
SenhaiQu <qusenhai2@gmail.com>
2022-06-02 20:52:42 -0400

    Version 5

--- PokeDexv5/src/app/components/pokemon/pokemon.component.ts
+++ PokeDexv5/src/app/components/pokemon/pokemon.component.ts
@@ -1,26 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 import {pokemon} from "../../pokemon";
 import {PokemonService} from "../../pokemon.service";
 import {HttpClient} from "@angular/common/http";
+
+
 
 @Component({
   selector: 'app-pokemon',
   templateUrl: './pokemon.component.html',
   styleUrls: ['./pokemon.component.css']
 })
-export class PokemonComponent implements OnInit {
-  private pokemons: pokemon[]=[];
+export class PokemonComponent  {
 
-  constructor(private http: HttpClient ) {
+
+
+
+
+
+
+
+
+ /*  public p:pokemon={
+  ID:1,
+  Name:"Picachu",
+  HP: 9999,
+  Attack:9999,
+  Defend:9999,
+  Picture:"picture_addresss"}*/
+
+
+
+
+
+  /*constructor(private http: HttpClient ) {
   }
   ngOnInit() {
     let resp= this.http.get("http://localhost:8081/pokemon");
+    //
     resp.subscribe((data) => console.log(data));
 
-
   }
-
-  /*public getPokemons():void{
+    public getPokemons():void{
     this.pokemonSevice.getPokemons().subscribe(
       (response: pokemon[]) => {
         this.pokemons=response;
