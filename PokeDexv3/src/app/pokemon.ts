863443724e7fbf1aa95d3959c519098badbcf0fa
James Villamayor <78101377+jsvillamayor@users.noreply.github.com>
2022-06-18 19:31:24 -0700

    Delete PokeDexv3 directory

--- PokeDexv3/src/app/pokemon.ts
+++
@@ -1,8 +0,0 @@
-export interface pokemon {
-  ID:number;
-  Name:string;
-  HP:number;
-  Attack:number;
-  Defend:number;
-  Picture:string;
-}
--- PokeDexv3/src/main/java/com/PokeDexv2/PokeDexv2/PokemonRepo/PokemonRepo.java
+++
@@ -1,8 +0,0 @@
-package com.PokeDexv2.PokeDexv2.PokemonRepo;
-import com.PokeDexv2.PokeDexv2.PokemonDB.Pokemons;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface PokemonRepo extends JpaRepository<Pokemons,Long>{
-
-}
