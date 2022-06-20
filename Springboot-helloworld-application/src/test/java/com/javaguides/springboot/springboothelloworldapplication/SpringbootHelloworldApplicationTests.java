6721d794de475993157a925e4046b09267830770
DavidLevant <104701459+DavidLevant@users.noreply.github.com>
2022-06-20 00:16:11 -0400

    Angular Practice
    This is a practice file to get the front end to talk to the back end

---
+++ Springboot-helloworld-application/src/test/java/com/javaguides/springboot/springboothelloworldapplication/SpringbootHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaguides.springboot.springboothelloworldapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
