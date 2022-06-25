ae48b1e5a5e886aa52263ea5746f26c795cee7b8
James Villamayor <78101377+jsvillamayor@users.noreply.github.com>
2022-06-24 21:59:44 -0700

    added delete function

--- PokedexBackend/Pokedex/src/main/java/com/group1/Pokedex/Controller/PokemonController.java
+++ PokedexBackend/Pokedex/src/main/java/com/group1/Pokedex/Controller/PokemonController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,5 +39,11 @@ public class PokemonController {
 	public ResponseEntity getPokemonByName(@PathVariable String name) {
 		return ResponseEntity.ok(pokemonService.getPokemonByName(name));
 	}
+	
+	@DeleteMapping("/{id}")
+    public ResponseEntity deletePokemon(@PathVariable String id) {
+        pokemonService.deletePokemon(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 	
 }
